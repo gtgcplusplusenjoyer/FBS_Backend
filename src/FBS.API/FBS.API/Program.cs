c158abc0f5d17f782395482f8785aaa2e8f01eb3
AlexFedorets <sakhafedorets904@gmail.com>
2026-04-25 19:56:54 +0300

    (FBS-01) feat: Add Migration

--- src/FBS.API/FBS.API/Program.cs
+++ src/FBS.API/FBS.API/Program.cs
@@ -1,14 +1,19 @@
+using FBS.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<FbsDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString(nameof(FbsDbContext)));
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
--- src/FBS.API/FBS.API/obj/Debug/net9.0/FBS.API.AssemblyInfo.cs
+++ src/FBS.API/FBS.API/obj/Debug/net9.0/FBS.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FBS.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ef6e71e2494f63f030f604da61b634fa0cb5171")]
 [assembly: System.Reflection.AssemblyProductAttribute("FBS.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FBS.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
