18c3a265755689749f70edd3b475d4303977a765
AlexFedorets <sakhafedorets904@gmail.com>
2026-04-25 23:52:21 +0300

    (FBS-01) feat: Add Validation

--- src/FBS.API/FBS.Infrastructure/obj/Debug/net9.0/FBS.Infrastructure.AssemblyInfo.cs
+++ src/FBS.API/FBS.Infrastructure/obj/Debug/net9.0/FBS.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FBS.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7718000ad90353e0f1892f0051abfb253a5017b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcaf1e17b651f1da451321ff5ca31658c01ef7b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("FBS.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FBS.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
