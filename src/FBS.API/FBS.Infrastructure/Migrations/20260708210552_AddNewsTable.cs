c0e4faf44617a5793b3906ead865b47054acdc22
AlexFedorets <sakhafedorets904@gmail.com>
2026-07-09 00:08:08 +0300

    (FBS-01) feat: add migration

---
+++ src/FBS.API/FBS.Infrastructure/Migrations/20260708210552_AddNewsTable.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FBS.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "News",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    ImageUrl = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: false),
+                    PublishedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_News", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_News_PublishedAt",
+                table: "News",
+                column: "PublishedAt");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_News_Title",
+                table: "News",
+                column: "Title");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "News");
+        }
+    }
+}
--- src/FBS.API/FBS.Infrastructure/obj/Debug/net9.0/FBS.Infrastructure.AssemblyInfo.cs
+++ src/FBS.API/FBS.Infrastructure/obj/Debug/net9.0/FBS.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FBS.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef7b9b68a89f6c0ff866e3cdcbc5834853359c30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f01ff663043d7782fdd40391823cf8be04304881")]
 [assembly: System.Reflection.AssemblyProductAttribute("FBS.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FBS.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
